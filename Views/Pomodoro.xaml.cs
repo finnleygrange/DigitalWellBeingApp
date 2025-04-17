925866612e69b3d7a7c90e77d4c461383809f49f
Finnley Grange <per.finnleygrange@icloud.com>
2025-04-17 16:06:03 +0100

    Added pomodoro functionality with different modes

--- Views/Pomodoro.xaml.cs
+++ Views/Pomodoro.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DigitalWellBeingApp.Views
 {
@@ -20,9 +21,105 @@ namespace DigitalWellBeingApp.Views
     /// </summary>
     public partial class Pomodoro : UserControl
     {
+
+        private DispatcherTimer _timer;
+        private TimeSpan _timeLeft;
+        private bool _isRunning = false;
+
         public Pomodoro()
         {
             InitializeComponent();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+
+            _timeLeft = TimeSpan.FromMinutes(25);
+            UpdateTimerDisplay();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (_timeLeft.TotalSeconds > 0)
+            {
+                _timeLeft = _timeLeft - TimeSpan.FromSeconds(1);
+                UpdateTimerDisplay();
+            }
+            else
+            {
+                _timer.Stop();
+                btnStart.Content = "START";
+                _isRunning = false;
+            }
+        }
+
+        private void UpdateTimerDisplay()
+        {
+            TimerText.Text = _timeLeft.ToString(@"mm\:ss");
+        }
+
+
+
+        private void btnPomodoro_Checked(object sender, RoutedEventArgs e)
+        {
+
+            if (TimerText == null)
+                return;
+
+            _timeLeft = TimeSpan.FromMinutes(25);
+            UpdateTimerDisplay();
+            btnReset_Click(sender, e);
+        }
+
+        private void btnShortBreak_Checked(object sender, RoutedEventArgs e)
+        {
+            _timeLeft = TimeSpan.FromMinutes(5);
+            UpdateTimerDisplay();
+            btnReset_Click(sender, e);
+        }
+        private void btnLongBreak_Checked(object sender, RoutedEventArgs e)
+        {
+            _timeLeft = TimeSpan.FromMinutes(15);
+            UpdateTimerDisplay();
+            btnReset_Click(sender, e);
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            _isRunning = false;
+            _timer.Stop();
+            btnStart.Content = "START";
+
+            if (btnPomodoro.IsChecked == true)
+            {
+                _timeLeft = TimeSpan.FromMinutes(25);
+            }
+            else if (btnShortBreak.IsChecked == true)
+            {
+                _timeLeft = TimeSpan.FromMinutes(5);
+            }
+            else if (btnLongBreak.IsChecked == true)
+            {
+                _timeLeft = TimeSpan.FromMinutes(15);
+            }
+
+            UpdateTimerDisplay();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isRunning)
+            {
+                _timer.Stop();
+                btnStart.Content = "START";
+            }
+            else
+            {
+                _timer.Start();
+                btnStart.Content = "PAUSE";
+            }
+
+            _isRunning = !_isRunning;
         }
     }
 }
