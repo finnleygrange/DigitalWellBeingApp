61171244a754c7629aad442d301120c3a4d94a62
Finnley Grange <per.finnleygrange@icloud.com>
2025-05-01 21:08:49 +0100

    Added Alarm Sound + Dev Testing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,59 +12,116 @@ using System.Windows.Controls;
 using DigitalWellBeingApp.Views;
 using LiveCharts;
 using LiveCharts.Wpf;
+using System.Runtime.InteropServices;
+using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace DigitalWellBeingApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        private DispatcherTimer _timer;
+        private string _currentAppTitle;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            PointLabel = chartPoint =>
-                string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
 
-            DataContext = this;
+            //PointLabel = chartPoint =>
+            //    string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+
+            //DataContext = this;
         }
 
-        public Func<ChartPoint, string> PointLabel { get; set; }
+        
 
-        private void PieChart_DataClick(object sender, ChartPoint chartPoint)
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            var chart = (LiveCharts.Wpf.PieChart)chartPoint.ChartView;
+            string title = GetActiveWindowTitle();
 
-            //clear selected slice.
-            foreach (PieSeries series in chart.Series)
-                series.PushOut = 0;
-
-            var selectedSeries = (PieSeries)chartPoint.SeriesView;
-            selectedSeries.PushOut = 8;
-        }
-
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            DragMove();
-        }
-
-        private void btnClose_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void btnMaximize_Click(object sender, RoutedEventArgs e)
-        {
-            if (WindowState == WindowState.Maximized)
+            if (title != _currentAppTitle)
             {
-                WindowState = WindowState.Normal;
-            }
-            else
-            {
-                WindowState = WindowState.Maximized;
+                _currentAppTitle = title;
+                //TrackApplicationUsage(title);  
             }
         }
+
+        public static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        //private void TrackApplicationUsage(string appTitle)
+        //{
+        //    var timeSpent = DateTime.Now - _appStartTimes[appTitle];
+        //    if (_appTimeSpent.ContainsKey(appTitle))
+        //    {
+        //        _appTimeSpent[appTitle] += timeSpent;
+        //    }
+        //    else
+        //    {
+        //        _appTimeSpent[appTitle] = timeSpent;
+        //    }
+
+        //    // Debugging: Output to the console
+        //    Debug.WriteLine($"{appTitle} spent: {timeSpent.TotalSeconds} seconds");
+        //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public Func<ChartPoint, string> PointLabel { get; set; }
+
+        //private void PieChart_DataClick(object sender, ChartPoint chartPoint)
+        //{
+        //    var chart = (LiveCharts.Wpf.PieChart)chartPoint.ChartView;
+
+        //    //clear selected slice.
+        //    foreach (PieSeries series in chart.Series)
+        //        series.PushOut = 0;
+
+        //    var selectedSeries = (PieSeries)chartPoint.SeriesView;
+        //    selectedSeries.PushOut = 8;
+        //}
+
 
         private void btnMinimize_Click(object sender, RoutedEventArgs e)
         {
--- Views/Pomodoro.xaml.cs
+++ Views/Pomodoro.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,21 +23,24 @@ namespace DigitalWellBeingApp.Views
     /// </summary>
     public partial class Pomodoro : UserControl
     {
-
         private DispatcherTimer _timer;
         private TimeSpan _timeLeft;
         private bool _isRunning = false;
+        private int _pomodoroCount = 0;
+
+        private bool IsTesting = true;
 
         public Pomodoro()
         {
             InitializeComponent();
 
-            _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
             _timer.Tick += Timer_Tick;
 
-            _timeLeft = TimeSpan.FromMinutes(25);
-            UpdateTimerDisplay();
+            SetTimer(TimeSpan.FromMinutes(25)); 
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -50,60 +55,48 @@ namespace DigitalWellBeingApp.Views
                 _timer.Stop();
                 btnStart.Content = "START";
                 _isRunning = false;
+
+                if (btnPomodoro.IsChecked == true)
+                    _pomodoroCount++;
+
+                PlayCustomAlarm();
+            }
+        }
+
+        private void SetTimer(TimeSpan duration)
+        {
+            _timeLeft = duration;
+            UpdateTimerDisplay();
+        }
+
+        private void PlayCustomAlarm()
+        {
+            string soundPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(),"Resources", "Sounds", "alarm.wav");
+
+            if (File.Exists(soundPath))
+            {
+                try
+                {
+                    SoundPlayer player = new SoundPlayer(soundPath);
+                    player.PlayLooping(); 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error playing sound: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Alarm sound file not found.");
             }
         }
 
         private void UpdateTimerDisplay()
         {
-            TimerText.Text = _timeLeft.ToString(@"mm\:ss");
-        }
-
-
-
-        private void btnPomodoro_Checked(object sender, RoutedEventArgs e)
-        {
-
-            if (TimerText == null)
-                return;
-
-            _timeLeft = TimeSpan.FromMinutes(25);
-            UpdateTimerDisplay();
-            btnReset_Click(sender, e);
-        }
-
-        private void btnShortBreak_Checked(object sender, RoutedEventArgs e)
-        {
-            _timeLeft = TimeSpan.FromMinutes(5);
-            UpdateTimerDisplay();
-            btnReset_Click(sender, e);
-        }
-        private void btnLongBreak_Checked(object sender, RoutedEventArgs e)
-        {
-            _timeLeft = TimeSpan.FromMinutes(15);
-            UpdateTimerDisplay();
-            btnReset_Click(sender, e);
-        }
-
-        private void btnReset_Click(object sender, RoutedEventArgs e)
-        {
-            _isRunning = false;
-            _timer.Stop();
-            btnStart.Content = "START";
-
-            if (btnPomodoro.IsChecked == true)
+            if (TimerText != null)
             {
-                _timeLeft = TimeSpan.FromMinutes(25);
+                TimerText.Text = _timeLeft.ToString(@"mm\:ss");
             }
-            else if (btnShortBreak.IsChecked == true)
-            {
-                _timeLeft = TimeSpan.FromMinutes(5);
-            }
-            else if (btnLongBreak.IsChecked == true)
-            {
-                _timeLeft = TimeSpan.FromMinutes(15);
-            }
-
-            UpdateTimerDisplay();
         }
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
@@ -121,5 +114,56 @@ namespace DigitalWellBeingApp.Views
 
             _isRunning = !_isRunning;
         }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            _isRunning = false;
+            _timer.Stop();
+            btnStart.Content = "START";
+
+            if (btnPomodoro.IsChecked == true)
+            {
+                SetTimer(TimeSpan.FromMinutes(25));
+            }
+            else if (btnShortBreak.IsChecked == true)
+            {
+                SetTimer(TimeSpan.FromMinutes(5));
+            }
+            else if (btnLongBreak.IsChecked == true)
+            {
+                SetTimer(TimeSpan.FromMinutes(15));
+            }
+        }
+
+        private void btnPomodoro_Checked(object sender, RoutedEventArgs e)
+        {
+            SetTimer(GetDurationForMode("Pomodoro"));
+        }
+
+        private void btnShortBreak_Checked(object sender, RoutedEventArgs e)
+        {
+            SetTimer(GetDurationForMode("ShortBreak"));
+        }
+
+        private void btnLongBreak_Checked(object sender, RoutedEventArgs e)
+        {
+            SetTimer(GetDurationForMode("LongBreak"));
+        }
+
+        private TimeSpan GetDurationForMode(string mode)
+        {
+            switch (mode)
+            {
+                case "Pomodoro":
+                    return IsTesting ? TimeSpan.FromSeconds(10) : TimeSpan.FromMinutes(25);
+                case "ShortBreak":
+                    return IsTesting ? TimeSpan.FromSeconds(5) : TimeSpan.FromMinutes(5);
+                case "LongBreak":
+                    return IsTesting ? TimeSpan.FromSeconds(8) : TimeSpan.FromMinutes(15);
+                default:
+                    return TimeSpan.FromMinutes(25);
+            }
+        }
     }
+
 }
